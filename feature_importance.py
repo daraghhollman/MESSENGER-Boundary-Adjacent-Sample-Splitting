52d8477a43a9bfbe386665142bb2c59e4086bcfd
Daragh Hollman <hollmandaragh@gmail.com>
2024-12-09 10:21:31 +0000

    Added function to plot the spread of spatial features

--- feature_importance.py
+++ feature_importance.py
@@ -3,85 +3,133 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import (ConfusionMatrixDisplay, accuracy_score,
-                             classification_report, confusion_matrix)
+from sklearn.metrics import (
+    ConfusionMatrixDisplay,
+    accuracy_score,
+    classification_report,
+    confusion_matrix,
+)
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 
-show_plots = True
 
-# Load data
-combined_features = pd.read_csv("/home/daraghhollman/Main/Work/mercury/DataSets/combined_features.csv")
+def main():
+    show_plots = True
 
-X = combined_features.drop(columns=["label", "Sample Start", "Sample End"])  # Features
-
-X = X.iloc[:, 1:]  # Remove the index column
-y = combined_features["label"]  # Target
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-
-random_forest = RandomForestClassifier(n_estimators=100, random_state=0)
-random_forest.fit(X_train, y_train)
-
-if show_plots:
-
-    y_pred = random_forest.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    print(f"Accuracy: {accuracy:.4f}")
-
-    importances = random_forest.feature_importances_
-    feature_names = X.columns
-
-    # Create a DataFrame for visualization
-    feature_importance_df = pd.DataFrame(
-        {"Feature": feature_names, "Importance": importances}
-    ).sort_values(by="Importance", ascending=False)
-
-    plt.figure(figsize=(10, 6))
-    sns.barplot(x="Importance", y="Feature", data=feature_importance_df)
-    plt.title("Feature Importance")
-    plt.show()
-
-    cm = confusion_matrix(y_test, y_pred)
-
-    print("\nConfusion Matrix\n")
-    print(cm)
-
-    print("\nClassification Report\n")
-    print(classification_report(y_test, y_pred))
-
-    cm_display = ConfusionMatrixDisplay(
-        cm, display_labels=["Magnetosheath", "Solar Wind"]
+    # Load data
+    combined_features = pd.read_csv(
+        "/home/daraghhollman/Main/Work/mercury/DataSets/combined_features.csv"
     )
-    cm_display.plot()
-    plt.show()
+
+    X = combined_features.drop(columns=["label", "Sample Start", "Sample End"])  # Features
+
+    X = X.iloc[:, 1:]  # Remove the index column
+    y = combined_features["label"]  # Target
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+    if input("Show training spread? [Y/n]\n > ") != "n":
+        Show_Training_Spread(X_train)
+
+    random_forest = RandomForestClassifier(n_estimators=100, random_state=0)
+    random_forest.fit(X_train, y_train)
+
+    if show_plots:
+
+        y_pred = random_forest.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        print(f"Accuracy: {accuracy:.4f}")
+
+        importances = random_forest.feature_importances_
+        feature_names = X.columns
+
+        # Create a DataFrame for visualization
+        feature_importance_df = pd.DataFrame(
+            {"Feature": feature_names, "Importance": importances}
+        ).sort_values(by="Importance", ascending=False)
+
+        plt.figure(figsize=(10, 6))
+        sns.barplot(x="Importance", y="Feature", data=feature_importance_df)
+        plt.title("Feature Importance")
+        plt.show()
+
+        cm = confusion_matrix(y_test, y_pred)
+
+        print("\nConfusion Matrix\n")
+        print(cm)
+
+        print("\nClassification Report\n")
+        print(classification_report(y_test, y_pred))
+
+        cm_display = ConfusionMatrixDisplay(
+            cm, display_labels=["Magnetosheath", "Solar Wind"]
+        )
+        cm_display.plot()
+        plt.show()
 
 
-if input("Save predictions to csv? [Y/n]\n > ") != "n":
-    truths = y_test  # What the correct label is
-    predictions = []  # What the random forest predicted
-    sheath_probability = []  # probability it is sheath
-    solar_wind_probability = []  # probability it is solar wind
+    if input("Save predictions to csv? [Y/n]\n > ") != "n":
+        truths = y_test  # What the correct label is
+        predictions = []  # What the random forest predicted
+        sheath_probability = []  # probability it is sheath
+        solar_wind_probability = []  # probability it is solar wind
 
-    # Create dataframe of how well the random forest performed
-    for i in tqdm(range(len(X_test))):
-        sample = X_test.iloc[i].to_frame().T
+        # Create dataframe of how well the random forest performed
+        for i in tqdm(range(len(X_test))):
+            sample = X_test.iloc[i].to_frame().T
 
-        prediction = random_forest.predict(sample)[0]
-        probabilities = random_forest.predict_proba(sample)
+            prediction = random_forest.predict(sample)[0]
+            probabilities = random_forest.predict_proba(sample)
 
-        predictions.append(prediction)
-        sheath_probability.append(probabilities[0][0])
-        solar_wind_probability.append(probabilities[0][1])
+            predictions.append(prediction)
+            sheath_probability.append(probabilities[0][0])
+            solar_wind_probability.append(probabilities[0][1])
 
-    prediction_data = pd.DataFrame(
-        {
-            "Truth": truths,
-            "Prediction": predictions,
-            "P(Magnetosheath)": sheath_probability,
-            "P(Solar Wind)": solar_wind_probability,
-        }
-    )
-    prediction_data.to_csv(
-        "/home/daraghhollman/Main/Work/mercury/DataSets/random_forest_predictions.csv"
-    )
+        prediction_data = pd.DataFrame(
+            {
+                "Truth": truths,
+                "Prediction": predictions,
+                "P(Magnetosheath)": sheath_probability,
+                "P(Solar Wind)": solar_wind_probability,
+            }
+        )
+        prediction_data.to_csv(
+            "/home/daraghhollman/Main/Work/mercury/DataSets/random_forest_predictions.csv"
+        )
+
+
+# Visualisation functions
+
+def Show_Training_Spread(training_data):
+    """A function to check if the training data is disperse spatially.
+
+    This is done by plotting distributions of spatial features.
+
+    """
+
+    features_to_test = [
+        "Heliocentric Distance (AU)",
+        "Local Time (hrs)",
+        "Latitude (deg.)",
+        "Magnetic Latitude (deg.)",
+        "X MSM' (radii)",
+        "Y MSM' (radii)",
+        "Z MSM' (radii)",
+    ]
+
+    for feature in features_to_test:
+        _, ax = plt.subplots()
+
+        ax.hist(training_data[feature], color="black")
+
+        ax.set_xlabel(feature)
+        ax.set_ylabel("# Events")
+
+        ax.margins(0)
+
+        plt.show()
+
+
+
+if __name__ == "__main__":
+    main()
